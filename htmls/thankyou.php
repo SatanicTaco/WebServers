f43ba21da173377accf58a369cc5b3f6572058c9
SatanicTaco <55203131+SatanicTaco@users.noreply.github.com>
2019-12-16 01:33:52 -0500

    yes

--- htmls/thankyou.php
+++ htmls/thankyou.php
@@ -13,22 +13,22 @@
                 $password = "password";
                 $dbname = "steaks";
                 //store form inputs to variables
-                $firstname = htmlspecialchars($_POST['firstname']);
-                $lastname = htmlspecialchars($_POST['lastname']);
-                $doneness = htmlspecialchars($_POST['doneness']);
-                $cut = htmlspecialchars($_POST['cut']);
+                $first = ($_GET['first']);
+                $last = ($_GET['last']);
+                $doneness = ($_GET['doneness']);
+                $cut = ($_GET['cut']);
             ?>
 
             <!--user confirmation message-->
-            <h3>Hello <?php echo $firstname ?> <?php echo $lastname ?>. </h3> </br>
+            <h3>Hello <?php echo $first ?> <?php echo $last ?>. </h3> </br>
             Your favorite steak is a <?php echo $doneness ?> <?php echo $cut ?>. </br> </br>
 
             <?php
                 //connection to db
                 $conn = mysqli_connect($servername, $username, $password, $dbname);
                 //insert data to db
-                $sql = "INSERT INTO steaks (firstname, lastname, cut, doneness)
-                    VALUES ('{$firstname}', '{$lastname}', '{$cut}', '{$doneness}')";
+                $sql = "INSERT INTO steaks (first, last, cut, doneness)
+                    VALUES ('{$first}', '{$last}', '{$cut}', '{$doneness}')";
                 //confirmation
                 if (mysqli_query($conn, $sql)) {
                     echo "Thank you for submitting your application.";
