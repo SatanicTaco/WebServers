e78a6924d399519e93813bc43ff414059ee279e1
SatanicTaco <55203131+SatanicTaco@users.noreply.github.com>
2019-12-16 01:55:48 -0500

    aa

--- scripts.js
+++ scripts.js
@@ -3,8 +3,6 @@ function validate(){
     var last = document.getElementById('last').value;
     var cut = document.getElementById('cut').value;
     var doneness = document.getElementById('doneness').value;
-    string = document.getElementById('cut').value;
-    string = document.getElementById('doneness').value;
 
     if(first=="" || last=="" || cut=="" || doneness==""){
         alert('Please fill in all fields');
