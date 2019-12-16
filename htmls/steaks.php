7b2e2dca236e9b830b7e08bf0abbe6e3d3435ed7
SatanicTaco <55203131+SatanicTaco@users.noreply.github.com>
2019-12-16 11:26:20 -0500

    q

--- htmls/steaks.php
+++ htmls/steaks.php
@@ -44,10 +44,10 @@
   <h3>Tell me more about yourself!</h3>
 
   <form action="thankyou.php" method="POST" onsubmit="return validate();">
-    <label for="name">First Name: </label><input id="first" type="text" /><br /><br />
-    <label for="name">Last Name: </label><input id="last" type="text" /><br /><br />
-    <label for="name">Favorite Cut: </label><input id="cut" type="text" /><br /><br />
-    <label for="name">Favorite Doneness: </label><input id="doneness" type="text" /><br /><br />
+    <label for="name">First Name: </label><input id="first" type="text" name="first" /><br /><br />
+    <label for="name">Last Name: </label><input id="last" type="text" name="last"/><br /><br />
+    <label for="name">Favorite Cut: </label><input id="cut" type="text" name="cut"/><br /><br />
+    <label for="name">Favorite Doneness: </label><input id="doneness" type="text" name="doneness"/><br /><br />
     <button id="submit">Submit</button>
   </form>
 
